d767359e61c08358369ecbde60e7dfa686d605fd
zhangrongjie <zsuzhang@qq.com>
2014-01-17 05:03:06 -0800

    init time lib

--- include/ze_lib.hrl
+++ include/ze_lib.hrl
@@ -6,6 +6,13 @@
 -endif.
 
 
- 
+-define(UTC_TO_LTC_DIFF,       8).
+-define(SECONDS_OF_MINUTE,      60).
+-define(SECONDS_OF_HOUR,        3600).
+-define(SECONDS_OF_DAY,         86400).
+-define(MAX_INTEGER,            999999999).
+-define(MAX_LONG_TIME,          2999999999).
+
+-define(GREGORIAN_LOCAL_TIME_DIFF, 62167219200). %%gregorian时间系统与Local时间系统的差值
 
 -endif.
---
+++ src/ze_time.erl
@@ -0,0 +1,71 @@
+%% @author rongjie
+%% zsuzhang (@) qq.com
+%% @doc @todo Add description to ze_time.
+
+
+-module(ze_time).
+-include("../include/ze_lib.hrl").
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([get_timestamp/0, get_longtimestamp/0]).
+-export([get_datetime_timestamp/1, get_timestamp_datetime/1]).
+
+%% for replace erlang:now(), because of erlang:now() function has performance problem
+%% 取得当前的时间戳
+get_timestamp() ->
+    {M, S, _} = os:timestamp(),
+    M * 1000000 + S.
+
+get_longtimestamp() ->
+    {M, S, Ms} = os:timestamp(),
+    M * 1000000000 + S*1000 + Ms div 1000.
+
+    
+
+
+%% 取本地标准时间对应的时间戳
+get_datetime_timestamp({{Y, M, D}, {HH, MM, SS}}) ->
+    [UtcDataTime] = calendar:local_time_to_universal_time_dst({{Y, M, D}, {HH, MM, SS}}),
+    xengine_time:datetime_to_timestamp(UtcDataTime).
+
+%% 取时间戳对应的本地标准时间
+get_timestamp_datetime(TimeStamp) ->
+    UtcDataTime = xengine_time:timestamp_to_datetime(TimeStamp),
+    calendar:universal_time_to_local_time(UtcDataTime).
+
+str_timestamp(TimeStamp) ->
+    {{Y, M, D}, {HH, MM, SS}} = get_timestamp_datetime(TimeStamp),
+    io_lib:format("~B-~B-~B ~B:~B:~B", [Y, M, D, HH, MM, SS]).
+
+get_timestamp_year_month_day(TimeStamp) ->
+    {{Y, M, D}, {_HH, _MM, _SS}} = get_timestamp_datetime(TimeStamp),
+    {Y,M,D}.
+
+
+%% 指定时间的凌晨0点的time
+midnight(TimeStamp) ->
+    {_Date, Time} = get_timestamp_datetime(TimeStamp),  
+    TimeStamp - calendar:time_to_seconds(Time).
+
+%% 今天凌晨0点的time
+midnight() ->
+    midnight(get_timestamp()).
+
+%% 明天开始的时间
+next_midnight() ->
+    midnight() + ?SECONDS_OF_DAY.
+
+next_midnight(TimeStamp) ->
+    midnight(TimeStamp) + ?SECONDS_OF_DAY.
+
+next_month_firstmidnight() ->
+    next_month_firstmidnight(get_timestamp()).
+next_month_firstmidnight(TimeStamp) ->
+    {{Y, M, D}, _} = get_timestamp_datetime(TimeStamp), 
+    midnight(TimeStamp) + ?SECONDS_OF_DAY * (calendar:last_day_of_the_month(Y, M)-D+1).
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
